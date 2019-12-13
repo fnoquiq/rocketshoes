3bffda757be98d41718be4ddd1ffeda91069dc53
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2019-12-13 19:44:30 -0300

    :rocket: Estiliza Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,106 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-infantil-disney-mickey-mouse/10/879-9800-010/879-9800-010_detalhe1.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-infantil-disney-mickey-mouse/10/879-9800-010/879-9800-010_detalhe1.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-infantil-disney-mickey-mouse/10/879-9800-010/879-9800-010_detalhe1.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-infantil-disney-mickey-mouse/10/879-9800-010/879-9800-010_detalhe1.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-infantil-disney-mickey-mouse/10/879-9800-010/879-9800-010_detalhe1.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-infantil-disney-mickey-mouse/10/879-9800-010/879-9800-010_detalhe1.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
